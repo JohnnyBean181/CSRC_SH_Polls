cf8a04bd1a6a73ecdb74a2f7811592dfd299739b
johnnybean <492569493@qq.com>
2024-12-24 13:54:11 +0800

    [dev] tutorial part 3

--- Poll2024/urls.py
+++ Poll2024/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = "Poll2024"
 urlpatterns = [
     path("", views.index, name="index"),
+    # ex: /poll2024/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /poll2024/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /poll2024/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- Poll2024/views.py
+++ Poll2024/views.py
@@ -1,8 +1,47 @@
-from django.shortcuts import render
+#from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
+# from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    """ full version
+    template = loader.get_template("Poll2024/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+    """
+
+    """ short version"""
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "Poll2024/index.html", context)
+    #output = ", ".join([q.question_text for q in latest_question_list])
+    #return HttpResponse(output)
+
+def detail(request, question_id):
+    #return HttpResponse("You're looking at question %s." % question_id)
+    """ full version
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "Poll2024/detail.html", {"question": question})
+    """
+
+    """ short version"""
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "Poll2024/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
